024e671f0dfd02698b9fd61fd33af3f8cd8871c4
zerostone <ziziakak@naver.com>
2023-01-10 10:49:11 +0900

    test

--- inventory/index.js
+++ inventory/index.js
@@ -5,5 +5,5 @@ document.oncontextmenu = function () {
 const ret = window.open(
   "./inventory.html",
   "_blank",
-  "fullscreen=yes, width=300, height=300, menubar=no, titlebar=no, resizable=no",
+  "width=300, height=300, menubar=no, titlebar=no, resizable=no",
 );
