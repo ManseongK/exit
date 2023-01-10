6bd9716260568198b6262f31bf69bb1964dadad5
zerostone <ziziakak@naver.com>
2023-01-10 11:43:55 +0900

    test

--- inventory/index.js
+++ inventory/index.js
@@ -2,10 +2,10 @@ document.oncontextmenu = function () {
   return false;
 };
 
-// document.addEventListener("keydown", (event) => {
-// if (event.key === "Enter") {
-// const ret = window.open("./inventory.html", "", "width=100, height=100");
-// }
-// });
+document.addEventListener("keydown", (e) => {
+  // if (e.key === "Enter") {
+  // const ret = window.open("./inventory.html", "", "width=100, height=100");
+  // }
+});
 
 // const ret = window.open("./inventory.html", "", "width=100, height=100");
