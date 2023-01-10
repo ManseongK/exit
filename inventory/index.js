fa8d34dcd1b3d5670b6062c574c0a3b7558b7d5c
zerostone <ziziakak@naver.com>
2023-01-10 10:48:34 +0900

    t

--- inventory/index.js
+++ inventory/index.js
@@ -1,9 +1,9 @@
-const ret = window.open(
-  "./inventory.html",
-  "",
-  "channelmode=yes,menubar=no,status=no,titlebar=no,toolbar=no",
-);
-
 document.oncontextmenu = function () {
   return false;
 };
+
+const ret = window.open(
+  "./inventory.html",
+  "_blank",
+  "fullscreen=yes, width=300, height=300, menubar=no, titlebar=no, resizable=no",
+);
