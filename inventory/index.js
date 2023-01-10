3a422fe7fb34de48eab3b70cbebeb6f552fb5806
zerostone <ziziakak@naver.com>
2023-01-10 11:38:47 +0900

    test

--- inventory/index.js
+++ inventory/index.js
@@ -4,7 +4,7 @@ document.oncontextmenu = function () {
 
 document.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
-    const ret = window.open("./inventory.html", "", "width=100, height=100");
+    // const ret = window.open("./inventory.html", "", "width=100, height=100");
   }
 });
 
