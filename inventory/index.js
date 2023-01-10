11f907a0530838741d590fa78e24721b9f1db492
zerostone <ziziakak@naver.com>
2023-01-10 10:30:24 +0900

    test

--- inventory/index.js
+++ inventory/index.js
@@ -1,4 +1,8 @@
-const ret = window.open("./inventory.html", "_parent", "resizable=yes");
+const ret = window.open(
+  "./inventory.html",
+  "_parent",
+  "channelmode=yes, menubar=no, status=no, titlebar=no, toolbar=no",
+);
 
 document.oncontextmenu = function () {
   return false;
